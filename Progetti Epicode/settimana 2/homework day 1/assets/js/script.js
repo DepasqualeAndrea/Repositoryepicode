ed4916f054b95897c8108662e72a99dac41932a0
Andrea De Pasquale <andre.dep1994@gmail.com>
2023-04-03 17:16:41 +0200

    esercizio JS day 1

---
+++ Progetti Epicode/settimana 2/homework day 1/assets/js/script.js
@@ -0,0 +1,9 @@
+document.write('<h1 id="output"></h1><div class="main"><h2>output scritto da JavaScript</h2><h2>Panterascritto da JavaScript</h2></div>')
+
+alert('questo è un alert scrivo da javaScript');
+alert(2 + 2);
+
+document.getElementById('output').innerHTML = 'iniziamo la nostra pratica'
+
+
+
---
+++ Progetti Epicode/settimana 2/homework day 1/assets/js/script2.js
@@ -0,0 +1,9 @@
+
+var nome = 'gatto'
+var nome1 = 'criceto'
+var nome2  = 'Opossum'
+var nome3 = 'pantegana'
+console.log(nome);
+console.log(nome1);
+console.log(nome2);
+console.log(nome3);
