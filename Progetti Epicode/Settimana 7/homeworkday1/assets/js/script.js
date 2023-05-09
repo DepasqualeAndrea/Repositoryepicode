05281a82c877ca09e5ed2f1bb99add1cd9ab1151
Andrea De Pasquale <andre.dep1994@gmail.com>
2023-05-09 14:11:55 +0200

    gna

---
+++ Progetti Epicode/Settimana 7/homeworkday1/assets/js/script.js
@@ -0,0 +1,5 @@
+const valueName = document.getElementById('valueName');
+const valueLastName = document.getElementById('valueLastName');
+const valueLocation = document.getElementById('valueLocation');
+const valueAge = document.getElementById('valueAge');
+const btnSub = document.getElementById('btnSub');
