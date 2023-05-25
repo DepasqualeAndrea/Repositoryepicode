969bd968587ea48cf6d20a74002de66a8ce4c023
Andrea De Pasquale <128024931+DepasqualeAndrea@users.noreply.github.com>
2023-05-25 15:31:20 +0200

    axa

--- Progetti Epicode/settmana 9/day 3/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class calculateTax {
-    constructor(_reddito, _codered, _inpsTax, _irpefTax) {
-        this.reddito = _reddito;
-        this.codered = _codered;
-        this.inpsTax = _inpsTax;
-        this.irpefTax = _irpefTax;
-    }
-}
