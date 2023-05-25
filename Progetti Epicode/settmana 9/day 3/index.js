6c471699d068bb0ae3e14cfaf9c164394c12eb62
DepasqualeAndrea <128024931+DepasqualeAndrea@users.noreply.github.com>
2023-05-25 13:36:37 +0200

    sxa

---
+++ Progetti Epicode/settmana 9/day 3/index.js
@@ -0,0 +1,9 @@
+"use strict";
+class calculateTax {
+    constructor(_reddito, _codered, _inpsTax, _irpefTax) {
+        this.reddito = _reddito;
+        this.codered = _codered;
+        this.inpsTax = _inpsTax;
+        this.irpefTax = _irpefTax;
+    }
+}
---
+++ Progetti Epicode/settmana 9/day 3/index.ts
@@ -0,0 +1,13 @@
+class calculateTax{reddito: number; codered: number; inpsTax: number; irpefTax: number;
+
+constructor (_reddito: number, _codered: number, _inpsTax: number, _irpefTax: number){
+    this.reddito = _reddito;
+    this.codered = _codered;
+    this.inpsTax = _inpsTax;
+    this.irpefTax = _irpefTax;
+    
+}
+
+
+
+}
