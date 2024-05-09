c5d7f18855bec8b341646f08fdce33c261d62837
Andrea De Pasquale <128024931+DepasqualeAndrea@users.noreply.github.com>
2024-05-09 16:18:38 +0200

    file readMe

--- Progetti Epicode/test Azienda/Test azienda/Test-azienda/src/main/java/AppRistorante/TestAziendaApplication.java
+++ Progetti Epicode/test Azienda/Test azienda/Test-azienda/src/main/java/AppRistorante/TestAziendaApplication.java
@@ -1,35 +1,27 @@
 package AppRistorante;
 
-import RandomTestGeneration.DatiCasualiGenerator;
-import gestioneOrdini.Ordine;
-import gestioneTavoli.Tavolo;
 import serviceOrdini.GestioneOrdineController;
 
 public class TestAziendaApplication {
 
 	public static void main(String[] args) {
 // metodo scanner, inserisce manualmente i dati in input (decommentare per utilizzare in locale, il test di simulazione manuale)
-//		GestioneOrdineController orderManager = new GestioneOrdineController();
-//		orderManager.gestisciOrdine();
+		GestioneOrdineController orderManager = new GestioneOrdineController();
+		orderManager.gestisciOrdine();
 
 		// METODO CON IL FAKER PER CREARE TAVOLI ORDINI TIPOLOGIA DI CLIENTI TEST
-		// Creazione del generatore casuale di dati
-		DatiCasualiGenerator generator = new DatiCasualiGenerator();
-
-		// Generazione di tavoli casuali con relativi ordini
-		for (Tavolo tavolo : generator.generaTavoliCasuali()) {
-			// Creazione di un gestore degli ordini
-			GestioneOrdineController gestioneOrdineService = new GestioneOrdineController();
-
-			// Creazione di un ordine per il tavolo
-			Ordine ordine = gestioneOrdineService.creaOrdine(tavolo);
-
-			// Stampa dei dettagli dell'ordine
-			gestioneOrdineService.stampaDettagliOrdine(tavolo, ordine);
-
-			// Aggiungi una linea vuota per separare gli ordini stampati
-			System.out.println();
-		}
+//		DatiCasualiGenerator generator = new DatiCasualiGenerator();
+//		
+//		for (Tavolo tavolo : generator.generaTavoliCasuali()) {
+//			GestioneOrdineController gestioneOrdineService = new GestioneOrdineController();
+//
+//			Ordine ordine = gestioneOrdineService.creaOrdine(tavolo);
+//
+//			gestioneOrdineService.stampaDettagliOrdine(tavolo, ordine);
+//
+//			
+//			System.out.println();
+//		}
 	}
 
 }
